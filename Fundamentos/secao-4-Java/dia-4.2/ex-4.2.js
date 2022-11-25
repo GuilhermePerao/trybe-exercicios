66a0923411a56ad6bce6e48f39fb15a0427be979
Guilherme Biancato Perão <guilhermebperao.gp@gmail.com>
2022-11-25 15:14:20 -0300

    exercícios dia 4.2

---
+++ Fundamentos/secao-4-Java/dia-4.2/ex-4.2.js
@@ -0,0 +1,10 @@
+let a = 8;
+let b = 9;
+
+if(a > b){
+    console.log(a + ' é maior que ' + b)
+} else if(a < b){
+    console.log(b + ' é maior que ' + a)
+} else {
+    console.log('Os dois números são iguais.')
+}
